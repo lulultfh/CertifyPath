3b996355fc2aa1a05a6eb69af7d32ba6fde8136a
lululthfh <punyalulu05@gmail.com>
2025-07-31 20:00:20 +0700

    feat: import article in main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import BootcampCards from "../components/bootcamp";
+import ArticleCards from "../components/article";
+import Navbar from "../components/navbar";
 import SidebarLayout from "../components/sidebar";
 
 export default function Home() {
   return (
-    <div>
+    <div style={{ display: "flex" }}>
       <SidebarLayout />
+      <div style={{ flex: 1, display: "flex", flexDirection: "column" }}>
+        <Navbar />
+      </div>
     </div>
   );
 }
