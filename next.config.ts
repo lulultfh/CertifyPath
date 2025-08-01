02daff96d810714b0182474d097cc5f487b11e5b
lululthfh <punyalulu05@gmail.com>
2025-08-01 18:16:14 +0700

    feat: update img

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'id.pinterest.com', "d3njjcbhbojbot.cloudfront.net",
-      "d15cw65ipctsrr.cloudfront.net", 'source.unsplash.com', 's3.amazonaws.com'],
+    domains: ['images.unsplash.com', 'id.pinterest.com', "d3njjcbhbojbot.cloudfront.net", 'www.shutterstock.com',
+      "d15cw65ipctsrr.cloudfront.net", 'source.unsplash.com', 's3.amazonaws.com', 'www.pexels.com'],
   },
 };
 
